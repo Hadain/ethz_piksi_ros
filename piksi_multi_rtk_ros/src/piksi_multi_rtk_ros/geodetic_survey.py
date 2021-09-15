ddde5720c58e5af3c7008572128883c0eaf8a9f7
Hadain <2489475+Hadain@users.noreply.github.com>
2021-09-15 16:26:21 +0200

    Update geodetic_survey.py

--- piksi_multi_rtk_ros/src/piksi_multi_rtk_ros/geodetic_survey.py
+++ piksi_multi_rtk_ros/src/piksi_multi_rtk_ros/geodetic_survey.py
@@ -310,6 +310,14 @@ class GeodeticSurvey:
         file_obj.write("altitude0: %.2f\n" % (alt0 - self.height_base_station_from_ground))
         file_obj.close()
         rospy.loginfo("ENU origin saved in \'" + desired_path + "\'")
+        
+    def rosparam_enu_origin_position(self, lat0, lon0, alt0):
+        # Current ENU origin to rosparam
+        rospy.set_param("surveyed_lat", lat0)
+        rospy.set_param("surveyed_lon", lon0)
+        rospy.set_param("surveyed_alt", (alt0 - self.height_base_station_from_ground))
+        rospy.loginfo("ENU origin saved in rosparam as surveyed_xxx")
+        
 
     # https://www.python.org/dev/peps/pep-0485/#proposed-implementation
     def is_close(self, a, b, rel_tol=1e-09, abs_tol=0.0):
